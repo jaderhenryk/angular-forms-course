48938501c36791444a362fbbef6d4493397c66fd
Jader Henrique de Paula <jaderhenryk@gmail.com>
2022-02-07 22:02:02 -0300

    Implement Login Reactive form
    Starting to implement Reactive version of login form.

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,6 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginReactiveComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    })
+  });
 
   constructor() {
 
