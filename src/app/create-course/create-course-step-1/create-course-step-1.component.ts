22673acf50f51567b43fffb4686f40eba31c7f2b
Jader Henrique de Paula <jaderhenryk@gmail.com>
2022-02-14 22:03:22 -0300

    Starting to create a multi-step form
    Starting to implement a Multi-Step Reactive form

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { courseTitleValidator } from '../../validators/course-title.validator';
 
 @Component({
   selector: 'create-course-step-1',
@@ -11,8 +10,33 @@ import {filter} from 'rxjs/operators';
 })
 export class CreateCourseStep1Component implements OnInit {
 
+  form = this.fb.group({
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [
+        courseTitleValidator(this.courseService)
+      ],
+      updateOn: 'blur'
+    }],
+    releasedAt: [new Date(), Validators.required],
+    downloadsAllowed: [false, Validators.requiredTrue],
+    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+  });
+
+  constructor(private fb: FormBuilder, private courseService: CoursesService) {
+
+  }
+
   ngOnInit() {
 
+  }
+
+  get courseTitle() {
+    return this.form.controls['title'];
   }
 
 }
